0f3cdacc75877958e721f45b92948542bf8da139
guilagarto <lagartoorei@gmail.com>
2026-09-17 01:43:00 -0300

    fix: injetado css via cdn para corrigir layout da hospedagem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+<!-- Injeção direta de CSS para garantir o design na Hostinger -->
+<link href="https://jsdelivr.net" rel="stylesheet">
 
         @fonts
 
